8fdb682da83b7e6554c5e29782f13756e883d1c1
Paul Whelan <paul.whelan@sentenial.com>
2021-11-18 10:54:38 +0000

    rolled the iat to 0

--- src/test/java/com/sentenial/sign/SignatureUtilsTest.java
+++ src/test/java/com/sentenial/sign/SignatureUtilsTest.java
@@ -15,11 +15,11 @@ public class SignatureUtilsTest {
     @Test
     public void testSignature() throws Exception {
 
-        String expectedSignture="ewogICJhbGciOiAiUlMyNTYiLAogICJraWQiOiAiMTMzNzQ3MTQxMjU1IiwKICAiaWF0IjogMTAsCiAgImlzcyI6ICJDPUdCLCBMPUxvbmRvbiwgT1U9TnVhcGF5IEFQSSwgTz1OdWFwYXksIENOPXlib3F5YXk5MnEiLAogICJiNjQiOiBmYWxzZSwKICAiY3JpdCI6IFsKICAgICJpYXQiLAogICAgImlzcyIsCiAgICAiYjY0IgogIF0KfQ..cC8RSP8H2mhO4KaXLRAecAWXhJKCsxEGZLdy96uNLwhD-ZdScIEHWqE60IqYQfPXRx3FEEl25hvJn9SexegHo7k7f13y7-qwtno_e2uCzSAPKw9ColknJtaQHMns4EoiUC1rC1WjFZqCQaNEC7uc1CG3mToX1JwPsuDCqp9V8-FcjUfloi5dN_1SrKkyAoBu80DHtVe8PGJ3-MgYZqOiVIS4aSOv9u-7hp-fRXzqZbXHnoXpVRLDckEP_sdYi18i6mBtf7QPupQNzvTGIEppZdp2WQVWkInqv3Dw61I8A5-qo0WPhiuVuhsT2Ookic9i1Gnj-VSBoj8w7WVDAuIRsg";
+        String expectedSignture="ewogICJhbGciOiAiUlMyNTYiLAogICJraWQiOiAiMTMzNzQ3MTQxMjU1IiwKICAiaWF0IjogMCwKICAiaXNzIjogIkM9R0IsIEw9TG9uZG9uLCBPVT1OdWFwYXkgQVBJLCBPPU51YXBheSwgQ049eWJvcXlheTkycSIsCiAgImI2NCI6IGZhbHNlLAogICJjcml0IjogWwogICAgImlhdCIsCiAgICAiaXNzIiwKICAgICJiNjQiCiAgXQp9..d_cZ46lwNiaFHAu_saC-Zz4rSzNbevWirO94EmBlbOwkB1L78vGbAnNjUsmFSU7t_HhL-cyMiQUDyRWswsEnlDljJsRi8s8ft48ipy2SMuZrjPpyYYMgink8nZZK7l-eFJcTiS9ZWezAAXF_IJFXSTO5ax9z6xty3zTNPNMV9W7aH8fEAvbUIiueOhH5xNHcsuqlOGygKdFz2rbjTGffoE_6zS4Dry-uX5mts2duLorobUimGsdlUcSM6P6vZEtcXaJCdjrT9tuFMh4CkX9nqk19Bq2z3i-SX4JCPvhD2r3ghRmX0gG08UcvyFVbrnVZJnpl4MU8V4Nr3-2M5URZOg";
         RSAPrivateKey key = SignatureUtils.loadTestPrivateKey();
         //simple test payload
         String payload = "{}";
-        String signature = SignatureUtils.createSignature(key, payload, "133747141255", 10, "C=GB, L=London, OU=Nuapay API, O=Nuapay, CN=yboqyay92q");
+        String signature = SignatureUtils.createSignature(key, payload, "133747141255", 0, "C=GB, L=London, OU=Nuapay API, O=Nuapay, CN=yboqyay92q");
         Assert.assertEquals(expectedSignture,signature);
         System.out.println("signature " + signature);
     }
